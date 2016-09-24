8b1481be14fddae4c87b01f65d2403fd8bc5411a
Ferry Laak <339188@student.fontys.nl>
2016-09-24 20:23:44 +0200

    Unit test start, methodes uit andere klassen kunnen niet worden aangeroepen.

---
+++ Game/UnitTestProject/EnemyTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Game;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class EnemyTest
+    {
+        public object cell { get; private set; }
+
+        [TestMethod]
+        public void AttackTest()
+        {
+            //?
+            Character player = new Character(Cell cell, 1000);
+            Character enemy = new Character(Cell cell, 300);
+            enemy.Attack();
+            Assert.AreNotEqual(player.Hitpoint, < player.Hitpoint);
+        }
+        [TestMethod]
+        public void MoveToRandomPositionTest()
+        {
+            //?
+            Character enemy = new Character(Cell cell, 300);
+            enemy.moveToRandomPositinon();
+            Assert.AreEqual(25, 1);
+
+            
+        }
+    }
+}
